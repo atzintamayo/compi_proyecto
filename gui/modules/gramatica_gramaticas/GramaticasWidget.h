3be3967273decc672c697bece3e15de0a325aea2
LuisMtz1717 <luismtzhdz250@gmail.com>
2025-12-02 19:39:12 -0600

    Update GramaticasWidget.h

--- gui/modules/gramatica_gramaticas/GramaticasWidget.h
+++ gui/modules/gramatica_gramaticas/GramaticasWidget.h
@@ -1,1 +1,21 @@
+#pragma once
+#include <QWidget>
+#include <QTableWidget>
+#include <QPushButton>
+#include "../../core/gramatica_gramaticas/Gramaticas.h"
 
+class GramaticasWidget : public QWidget {
+    Q_OBJECT
+public:
+    explicit GramaticasWidget(QWidget *parent = nullptr);
+
+private slots:
+    void onLoadFile();
+
+private:
+    QTableWidget *table;
+    QPushButton *btnLoad;
+    GrammarAnalyzer analyzer;
+
+    void fillTable(const std::vector<RegexResult> &data);
+};
